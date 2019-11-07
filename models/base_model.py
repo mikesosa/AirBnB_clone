8d277122799e91965ddf726d246851f63b409dcf
Michael <michael.sosa@holbertonschool.com>
2019-11-07 18:40:38 -0500

    [Fix] adding kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,31 @@ Base class mother of all classes and chikens
 ==============================================
 """
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
-
+from datetime import timedelta
 
 class BaseModel:
     """Mother of classes and chickens"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """The constructor"""
-
-        self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "updated_at":
+                    # reversing iso format to datetime
+                    # we did not check created_at because its
+                    # overwritten at the end.
+                    value, _, m_seconds = value.partition(".")
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
+                    m_seconds = int(m_seconds.rstrip("Z"), 10)
+                    value += timedelta(microseconds = m_seconds)
+                setattr(self, key, value)
+        else:
+            self.updated_at = datetime.now()
+        self.id = str(uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
 
     def __str__(self):
         """print information """
@@ -36,7 +48,7 @@ class BaseModel:
         for key, value in self.__dict__.items():
             my_dic["__class__"] = self.__class__.__name__
             if key == "created_at" or key == "updated_at":
-                my_dic[key] = value.isoformat()
+                my_dic[key] = value.isoformat() # convert to string
             else:
                 my_dic[key] = value
         return my_dic
