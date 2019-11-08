e6f6307018a2b9ca55b360c75b5f083b4dd0b313
Yesidh <944@holbertonschool.com>
2019-11-08 08:31:03 -0500

    [Fix]: changing the __str__ format to print the string, adding if condition for __class__ in the file base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ from uuid import uuid4
 from datetime import datetime
 from datetime import timedelta
 
+
 class BaseModel:
     """Mother of classes and chickens"""
 
@@ -16,6 +17,8 @@ class BaseModel:
         """The constructor"""
         if kwargs:
             for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
                 if key == "updated_at":
                     # reversing iso format to datetime
                     # we did not check created_at because its
@@ -23,19 +26,18 @@ class BaseModel:
                     value, _, m_seconds = value.partition(".")
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
                     m_seconds = int(m_seconds.rstrip("Z"), 10)
-                    value += timedelta(microseconds = m_seconds)
-                setattr(self, key, value)
+                    value += timedelta(microseconds=m_seconds)
+                    setattr(self, key, value)
         else:
             self.updated_at = datetime.now()
         self.id = str(uuid4())
         self.created_at = datetime.now()
 
-
     def __str__(self):
-        """print information """
-        string = "[" + type(self).__name__ + "]"
-        string += " (" + self.id + ") " + str(self.__dict__)
-        return string
+        """print information"""
+
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     str(self.__dict__))
 
     def save(self):
         """update the attribute updated_at"""
@@ -48,7 +50,7 @@ class BaseModel:
         for key, value in self.__dict__.items():
             my_dic["__class__"] = self.__class__.__name__
             if key == "created_at" or key == "updated_at":
-                my_dic[key] = value.isoformat() # convert to string
+                my_dic[key] = value.isoformat()  # convert to string
             else:
                 my_dic[key] = value
         return my_dic
